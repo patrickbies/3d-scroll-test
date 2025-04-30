39100cc7db80da81166c1e1f3b4150771a8781e2
patrickbies <patjbies@gmail.com>
2025-04-29 22:00:30 -0400

    changes...

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,32 +1,57 @@
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useMemo, useRef, useState } from "react";
 import Lenis from "@studio-freight/lenis";
 import Header from "./Header";
 import { useGLTF } from "@react-three/drei";
 import Starfield from "./Starfield";
+import { Group, MathUtils, Object3D, Vector3 } from "three";
+import { lerp } from "three/src/math/MathUtils.js";
 
-const DNA = ({ progress }: { progress: number }) => {
+const DNA = ({ progress }: {progress : number})=> {
   const { scene } = useGLTF('/dna.glb');
+  const helix = useMemo(() => {
+    const g = scene.clone();
+    g.scale.setScalar(0.1);
+    return g;
+  }, [scene]);
 
-  // scene.position.y = 0;
+  const wrapper = useRef<Group>(null!);
+  const spin = useRef<Object3D>(null!);
 
-  // useFrame(() => {
-  //     scene.rotation.x = 0
-  //     scene.rotation.z = 1
-  //     scene.rotation.y = progress * Math.PI * 2;
-  //     scene.position.z = -50;
-  // });
+  const startPos = new Vector3(0,  0.5, -2);
+  const endPos   = new Vector3(-0.5,  -0.5,  -2.5);
 
-  return <primitive object={scene} dispose={null} />;
-};
+  useFrame((_, dt) => {
+    const t = MathUtils.clamp(progress / 200, 0, 1);
 
-function ScrollCamera({ progress }: { progress: number }) {
+    spin.current.rotation.x += dt * 0.5;
+
+    wrapper.current.position.lerpVectors(startPos, endPos, t);
+    wrapper.current.rotation.z = t * Math.PI * 0.5;
+    wrapper.current.rotation.x = -t * Math.PI / 4;
+  });
+
+  return (
+    <group ref={wrapper}>
+      <group ref={spin}>
+        <primitive object={helix} dispose={null} />
+      </group>
+    </group>
+  );
+}
+
+const ScrollCamera = ({ progress }: { progress: number }) => {
   const { camera } = useThree();
   useFrame(() => {
     if (progress <= 200) {
-      const t = progress/200 * Math.PI / 2; 
-      camera.rotation.x = -t/2;
-      camera.position.set(0, Math.cos(t), -1.5*Math.sin(t));
+      const t = ((progress / 200) * Math.PI) / 2;
+      camera.rotation.x = -t / 2;
+      camera.position.set(0, Math.cos(t), -1.5 * Math.sin(t));
+    }
+    if (progress >= 550 && progress <= 750) {
+      const t = (progress - 550) / 200
+
+      
     }
   });
   return null;
@@ -51,14 +76,13 @@ const HeroSection = () => {
 
     lenis.on("scroll", ({ progress }: { progress: number }) => {
       setProgress(progress * vh);
-      console.log(progress * vh);
     });
 
     return () => lenis.destroy();
   }, []);
 
   return (
-    <div className="overflow-hidden" style={{height: vh}}>
+    <div className="overflow-hidden" style={{ height: vh }}>
       <Header />
       {/* Three.js scene */}
       <Canvas
@@ -69,19 +93,54 @@ const HeroSection = () => {
         <ambientLight intensity={0.4} />
         <directionalLight position={[5, 5, 5]} intensity={0.8} />
         <Suspense fallback={null}>
-          <ScrollCamera progress={progress}/>
-          <Starfield count={200} minspeed={0.05} maxspeed={0.15} halfSize={5} tiltDeg={20} size={0.1} />
+          <ScrollCamera progress={progress} />
+          <Starfield
+            count={200}
+            minspeed={0.05}
+            maxspeed={0.15}
+            halfSize={5}
+            tiltDeg={20}
+            size={0.1}
+          />
           <DNA progress={progress} />
         </Suspense>
       </Canvas>
 
       {/* Text overlay */}
-      { progress <= 100 &&
-      <div className="fixed pointer-events-none w-full h-screen justify-center flex items-center" style={{opacity: Math.max(1-progress/100, 0)}}>
-        <h1 className="playfair-display-bold text-[5rem] text-white">
-          Lorem ipsum dolor sit <br />adipisicing elit.
-        </h1>
-      </div>
+      {progress <= 100 && (
+        <div
+          className="fixed pointer-events-none w-full h-screen justify-center flex items-center"
+          style={{ opacity: Math.max(1 - progress / 100, 0) }}
+        >
+          <h1 className="playfair-display-bold text-[5rem] text-white">
+            Lorem ipsum dolor sit <br />
+            adipisicing elit.
+          </h1>
+        </div>
+      )}
+      {
+        progress >= 200 && (
+          <div
+          className="absolute top-[260px] pointer-events-none w-full h-screen justify-end flex items-center"
+          style={{opacity: progress < 350 ? (progress - 200) / 50 : (1 - (progress - 350) / 50)}}
+        >
+          <h1 className="playfair-display-medium text-[1.5rem] w-[40vw] pr-[10vw] text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi doloribus tempora vel cum sapiente veritatis hic, modi aperiam ipsum repudiandae enim dolorem, quasi obcaecati corrupti!
+          </h1>
+        </div>
+        )
+      }
+      {
+        progress >= 400 && (
+          <div
+          className="absolute top-[460px] pointer-events-none w-full h-screen justify-end flex items-center"
+          style={{opacity: progress < 550 ? (progress - 400) / 50 : (1 - (progress - 550) / 50)}}
+        >
+          <h1 className="playfair-display-medium text-[1.5rem] w-[40vw] pr-[10vw] text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi doloribus tempora vel cum sapiente veritatis hic, modi aperiam ipsum repudiandae enim dolorem, quasi obcaecati corrupti!
+          </h1>
+        </div>
+        )
       }
     </div>
   );
